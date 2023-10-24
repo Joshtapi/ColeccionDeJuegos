e744ad0941a33374b626b6c9c8d35f918c111888
JoshTapi <joshua.tapia@tecsup.edu.pe>
2023-10-24 15:58:10 -0500

    Initial Commit

---
+++ TapiaColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TapiaColeccionDeJuegos
+//
+//  Created by Joshua Tapia on 24/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
